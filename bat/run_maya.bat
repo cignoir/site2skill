9581cffe01c6b4b92cdd485faf0e794c469b4c44
cignoir <cignoir@gmail.com>
2026-01-16 11:28:01 +0900

    Update run_maya.bat

--- bat/run_maya.bat
+++ bat/run_maya.bat
@@ -1,11 +1,11 @@
 REM Commands
-python -m site2skill.main "https://help.autodesk.com/cloudhelp/2025/JPN/Maya-Tech-Docs/Commands/index_all.html" maya2025_cmd_mel --output output
-python -m site2skill.main "https://help.autodesk.com/cloudhelp/2025/JPN/Maya-Tech-Docs/CommandsPython/index_all.html" maya2025_cmd_python --output output
-python -m site2skill.main "https://help.autodesk.com/cloudhelp/2026/JPN/Maya-Tech-Docs/Commands/index_all.html" maya2026_cmd_mel --output output
-python -m site2skill.main "https://help.autodesk.com/cloudhelp/2026/JPN/Maya-Tech-Docs/CommandsPython/index_all.html" maya2026_cmd_python --output output
+python -m site2skill.main "https://help.autodesk.com/cloudhelp/2025/JPN/Maya-Tech-Docs/Commands/index_all.html" maya2025-cmd-mel --output output
+python -m site2skill.main "https://help.autodesk.com/cloudhelp/2025/JPN/Maya-Tech-Docs/CommandsPython/index_all.html" maya2025-cmd-python --output output
+python -m site2skill.main "https://help.autodesk.com/cloudhelp/2026/JPN/Maya-Tech-Docs/Commands/index_all.html" maya2026-cmd-mel --output output
+python -m site2skill.main "https://help.autodesk.com/cloudhelp/2026/JPN/Maya-Tech-Docs/CommandsPython/index_all.html" maya2026-cmd-python --output output
 
 REM API Ref
-python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=MAYA_API_REF_cpp_ref_index_html" maya2025_cpp --browser --sidebar-selector "#nav-tree" --output output
-python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2026/ENU/?guid=MAYA_API_REF_cpp_ref_index_html" maya2026_cpp --browser --sidebar-selector "#nav-tree" --output output
-python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=MAYA_API_REF_py_ref_index_html" maya2025_python --browser --sidebar-selector "#nav-tree" --output output
-python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2026/ENU/?guid=MAYA_API_REF_py_ref_index_html" maya2026_python --browser --sidebar-selector "#nav-tree" --output output
+python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=MAYA_API_REF_cpp_ref_index_html" maya2025-cpp --browser --sidebar-selector "#nav-tree" --output output
+python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2026/ENU/?guid=MAYA_API_REF_cpp_ref_index_html" maya2026-cpp --browser --sidebar-selector "#nav-tree" --output output
+python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2025/ENU/?guid=MAYA_API_REF_py_ref_index_html" maya2025-python --browser --sidebar-selector "#nav-tree" --output output
+python -m site2skill.main "https://help.autodesk.com/view/MAYADEV/2026/ENU/?guid=MAYA_API_REF_py_ref_index_html" maya2026-python --browser --sidebar-selector "#nav-tree" --output output
